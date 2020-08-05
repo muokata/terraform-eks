edb35d83ca0a12eda7afa43d28accc975152b6ee
Allan Graham <allan@nomadology.net>
2020-08-05 16:52:58 +0000

    cluster_version var

--- variables.tf
+++ variables.tf
@@ -3,6 +3,10 @@ variable "key_file_path" {}
 variable "profile" {}
 #variable "muokata_net_zone_id" {}
 
+variable "cluster_version" {
+        default = "1.16"
+}
+
 variable "aws_region" {
   description = "AWS region to launch servers."
   default     = "us-east-1"
